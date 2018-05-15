47b2937b7ae68366495534c1e45dd2569d08e70a
fl-mann <florian.mann1@gmail.com>
2018-05-15 14:06:29 +0200

    Fixed dynamic size when receiving shorter messages

--- mrt-mel3/mrt-mel3.h
+++ mrt-mel3/mrt-mel3.h
@@ -9,6 +9,6 @@ const DCB dcbPeakTech4390 = { .BaudRate = CBR_9600,.Parity = NOPARITY,.StopBits 
 const BYTE D = 0x44; // 44 in Hex is the char 'D'
 
 void PrintCommState(const DCB dcb);
-float getResistanceFromString(const char* string);
+float getResistanceFromString(const char* string, int len);
 float convertResistanceToCelcius(const int res);
 int main();
